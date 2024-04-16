c1ff39d7745776eb963e87eb9a4002dc43489150
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 06:33:00 +0700

    Create starter project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf2/SpringBootGeneratePdf2Application.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootgeneratepdf2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGeneratePdf2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootGeneratePdf2Application.class, args);
+    }
+
+}
