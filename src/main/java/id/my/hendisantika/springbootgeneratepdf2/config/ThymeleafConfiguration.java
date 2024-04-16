287eea233c18876786b72109f848f231bbdf9e49
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 06:38:11 +0700

    Add ThymeleafConfiguration 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf2/config/ThymeleafConfiguration.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.springbootgeneratepdf2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-generate-pdf2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/17/24
+ * Time: 06:37
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class ThymeleafConfiguration {
+
+    @Bean
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver pdfTemplateResolver = new ClassLoaderTemplateResolver();
+        pdfTemplateResolver.setPrefix("pdf-templates/");
+        pdfTemplateResolver.setSuffix(".html");
+        pdfTemplateResolver.setTemplateMode("HTML5");
+        pdfTemplateResolver.setCharacterEncoding("UTF-8");
+        pdfTemplateResolver.setOrder(1);
+        return pdfTemplateResolver;
+    }
+}
