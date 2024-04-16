d691804e459e88a71929b38b723f9951a0b0a8cc
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 06:36:37 +0700

    Add generatePdfFile in PdfGenerateService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootgeneratepdf2/service/PdfGenerateService.java
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf2/service/PdfGenerateService.java
@@ -1,10 +1,17 @@
 package id.my.hendisantika.springbootgeneratepdf2.service;
 
+import com.lowagie.text.DocumentException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +31,23 @@ public class PdfGenerateService {
 
     @Value("${pdf.directory}")
     private String pdfDirectory;
+
+    public void generatePdfFile(String templateName, Map<String, Object> data, String pdfFileName) {
+        Context context = new Context();
+        context.setVariables(data);
+
+        String htmlContent = templateEngine.process(templateName, context);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(pdfDirectory + pdfFileName);
+            ITextRenderer renderer = new ITextRenderer();
+            renderer.setDocumentFromString(htmlContent);
+            renderer.layout();
+            renderer.createPDF(fileOutputStream, false);
+            renderer.finishPDF();
+        } catch (FileNotFoundException e) {
+            log.error(e.getMessage(), e);
+        } catch (DocumentException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
 }
