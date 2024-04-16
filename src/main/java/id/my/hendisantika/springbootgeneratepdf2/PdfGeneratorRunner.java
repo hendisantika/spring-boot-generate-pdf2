5419d520ea25b9f57a14fd5e2bf6befe896e7f5f
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 06:39:22 +0700

    Add init data in PdfGeneratorRunner 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootgeneratepdf2/PdfGeneratorRunner.java
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf2/PdfGeneratorRunner.java
@@ -1,9 +1,16 @@
 package id.my.hendisantika.springbootgeneratepdf2;
 
+import id.my.hendisantika.springbootgeneratepdf2.entity.Customer;
+import id.my.hendisantika.springbootgeneratepdf2.entity.QuoteItem;
 import id.my.hendisantika.springbootgeneratepdf2.service.PdfGenerateService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,9 +23,47 @@ import org.springframework.stereotype.Component;
  * To change this template use File | Settings | File Templates.
  */
 @Component
+@RequiredArgsConstructor
 public class PdfGeneratorRunner implements CommandLineRunner {
 
-    @Autowired
-    private PdfGenerateService pdfGenerateService;
+    private final PdfGenerateService pdfGenerateService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, Object> data = new HashMap<>();
+        Customer customer = new Customer();
+        customer.setCompanyName("Simple Solution");
+        customer.setContactName("John Doe");
+        customer.setAddress("123, Simple Street");
+        customer.setEmail("john@simplesolution.dev");
+        customer.setPhone("123 456 789");
+        data.put("customer", customer);
+
+        List<QuoteItem> quoteItems = new ArrayList<>();
+        QuoteItem quoteItem1 = new QuoteItem();
+        quoteItem1.setDescription("Test Quote Item 1");
+        quoteItem1.setQuantity(1);
+        quoteItem1.setUnitPrice(100.0);
+        quoteItem1.setTotal(100.0);
+        quoteItems.add(quoteItem1);
+
+        QuoteItem quoteItem2 = new QuoteItem();
+        quoteItem2.setDescription("Test Quote Item 2");
+        quoteItem2.setQuantity(4);
+        quoteItem2.setUnitPrice(500.0);
+        quoteItem2.setTotal(2000.0);
+        quoteItems.add(quoteItem2);
+
+        QuoteItem quoteItem3 = new QuoteItem();
+        quoteItem3.setDescription("Test Quote Item 3");
+        quoteItem3.setQuantity(2);
+        quoteItem3.setUnitPrice(200.0);
+        quoteItem3.setTotal(400.0);
+        quoteItems.add(quoteItem3);
+
+        data.put("quoteItems", quoteItems);
+
+        pdfGenerateService.generatePdfFile("quotation", data, "quotation.pdf");
+    }
 
 }
