75ae7aacc1e9511badb411df97f3b4e85222d5f0
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 07:01:16 +0700

    Add Employee Entity 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootgeneratepdf2/entity/Employee.java
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf2/entity/Employee.java
@@ -1,5 +1,11 @@
 package id.my.hendisantika.springbootgeneratepdf2.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : spring-boot-generate-pdf2
@@ -10,5 +16,15 @@ package id.my.hendisantika.springbootgeneratepdf2.entity;
  * Time: 06:57
  * To change this template use File | Settings | File Templates.
  */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
+
+    @Id
+    private Integer empId;
+    private String empName;
+    private Double empSal;
+    private String empDept;
 }
