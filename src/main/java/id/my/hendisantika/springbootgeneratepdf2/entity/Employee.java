fc7be2db8d6b3ff25c310f7094cc5500e938a4b3
hendisantika <hendisantika@yahoo.co.id>
2024-04-17 07:00:23 +0700

    Add required libs 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf2/entity/Employee.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.springbootgeneratepdf2.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-generate-pdf2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 4/17/24
+ * Time: 06:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class Employee {
+}
