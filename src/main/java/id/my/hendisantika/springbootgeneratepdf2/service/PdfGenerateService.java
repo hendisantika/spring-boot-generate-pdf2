e45dd3c57988699e75e33df5dafb1aecdd2a334b
hendisantika <hendisantika@yahoo.co.id>
2024-08-06 06:44:37 +0700

    Add export in PdfGenerateService 🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/springbootgeneratepdf2/service/PdfGenerateService.java
+++ src/main/java/id/my/hendisantika/springbootgeneratepdf2/service/PdfGenerateService.java
@@ -1,6 +1,13 @@
 package id.my.hendisantika.springbootgeneratepdf2.service;
 
+import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -10,6 +17,7 @@ import org.xhtmlrenderer.pdf.ITextRenderer;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -50,5 +58,32 @@ public class PdfGenerateService {
         } catch (DocumentException e) {
             log.error(e.getMessage(), e);
         }
+    }
+
+    public void export(HttpServletResponse httpServletResponse) throws IOException {
+
+        Document document = new Document(PageSize.A4);
+        PdfWriter.getInstance(document, httpServletResponse.getOutputStream());
+
+        document.open();
+        Font fontTitle = FontFactory.getFont(FontFactory.COURIER_BOLD);
+        fontTitle.setSize(16);
+        fontTitle.setColor(108, 142, 163);
+
+        Paragraph paragraph = new Paragraph("Generate a PDF Using Spring Boot", fontTitle);
+        paragraph.setAlignment(Paragraph.ALIGN_CENTER);
+
+        Font fontParagraph = FontFactory.getFont(FontFactory.HELVETICA);
+        fontParagraph.setSize(12);
+        fontParagraph.setColor(153, 153, 255);
+
+        Paragraph headingPara = new Paragraph("This is Heading for a Paragraph.", fontParagraph);
+        headingPara.setAlignment(Paragraph.ALIGN_LEFT);
+
+        document.add(paragraph);
+        document.add(headingPara);
+
+        document.close();
+
     }
 }
